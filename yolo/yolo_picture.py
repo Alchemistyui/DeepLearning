9f490f558a5239a5a332a3b50347bd91fdcaefe9
Alchemist <1319525622@qq.com>
2018-04-16 12:55:28 +0800

    add yolo

---
+++ yolo/yolo_picture.py
@@ -0,0 +1,4 @@
+from cnn_yolo import testmainpicture
+in_file = "/Users/ryshen/Desktop/yolo-master/images/timg3.jpg"
+out_file = "/Users/ryshen/Desktop/yolo-master/out/testemm.jpg"
+testmainpicture(in_file,out_file)
