6de092858444a8ed7bac8c526dfc0a8a25a935e7
Alchemist <1319525622@qq.com>
2018-04-19 08:37:20 +0800

    add shared functional

--- keras_mimo_functional.py
+++ keras_mimo_functional.py
@@ -1,57 +1,3 @@
-# import keras
-# from keras.layers import Input, Embedding, LSTM, Dense
-# from keras.models import Model
-# import numpy as np
-
-
-# main_data
-# 主数据集为10000*100的二维数组，意味着100个特征
-# 标签为10000*1的二维数组，共有10种输出结果
-# main_x = np.random.random((10000,100))
-# main_y = keras.utils.to_categorical(np.random.randint(10,size = (10000,1)))
-# #additional_data
-
-
-# # 主数据集和额外的数据集的输入的特征张量的数据集个数相等，也就是行数相等；
-# add_x = np.random.random((10000,10))
-# add_y = keras.utils.to_categorical(np.random.randint(10,size = (10000,1)))
-
-# # 设定主要输入的张量，并命名main_input
-# # input里shape=(特征个数,)
-# main_input = Input(shape=(100,), dtype='int32', name='main_input')
-
-# # 嵌入生成512列维度的张量
-# x = Embedding(output_dim=512, input_dim=10000, input_length=100)(main_input)
-# #使用LSTM模型
-# lstm_out = LSTM(32)(x)
-
-# # 插入一个额外的损失，使得即使在主损失很高的情况下，LSTM和Embedding层也可以平滑的训练
-# auxiliary_output = Dense(1, activation='sigmoid', name='aux_output')(lstm_out)
-
-# #额外的输入数据
-# auxiliary_input = Input(shape=(10,), name='aux_input')
-
-# #将LSTM得到的张量与额外输入的数据串联起来，横向连接
-# x = keras.layers.concatenate([lstm_out, auxiliary_input])
-# #建立一个深层连接的网络
-# # We stack a deep densely-connected network on top
-# x = Dense(64, activation='relu')(x)
-# x = Dense(64, activation='relu')(x)
-# x = Dense(64, activation='relu')(x)
-
-# #得到主数据集输出的张量，与输入的主数据集标签数据集的标签类相等
-# main_output = Dense(1, activation='sigmoid', name='main_output')(x)
-
-# # 整个2输入，2输出的模型
-# model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output, auxiliary_output])
-
-# # 给额外的损失赋0.2的权重。我们可以通过关键字参数loss_weights或loss来为不同的输出设置不同的损失函数或权值
-# # 此处给loss传递单个损失函数，这个损失函数会被应用于所有输出上
-# model.compile(optimizer='rmsprop', loss='binary_crossentropy',loss_weights=[1., 0.2])
-
-# # 传递训练数据和目标值训练该模型
-# model.fit([main_x, add_x], [main_y, main_y],epochs=10, batch_size=128)
-
 
 import keras
 from keras.layers import Input, Embedding, LSTM, Dense
@@ -99,7 +45,3 @@ model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output, auxili
 model.compile(optimizer='rmsprop', loss='binary_crossentropy',loss_weights=[1., 0.2])
 # 传递训练数据和目标值训练该模型
 model.fit([main_x, add_x], [main_y, main_y],epochs=10, batch_size=128)
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
