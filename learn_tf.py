7b5e6a9fe6e8db5b6a01d99e068dd98ce0dea4cf
Alchemist <1319525622@qq.com>
2018-08-27 13:49:38 +0800

    add

--- learn_tf.py
+++ learn_tf.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+# import numpy as np
 
 # 此时只是创建了一个graph，而并不会真正的输出结果
 a = tf.add(3, 5)
@@ -28,3 +29,31 @@ with tf.Session() as sess:
 
 
 # tf也能让计算图的各个子图并行，可分配给不同特定GPU
+
+# 在特定GPU上创建graph,实例化所有结点
+with tf.device('/gpu:2'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0], name='b')
+    # c = tf.matmul(a, b)
+
+# 。。。。
+
+
+# tf一般是在默认图里运行，即sess.run()，但有时想创建多个图，如对同一个任务，想同时训练好几个模型
+# 为了防止混用，最好先保存下默认图方便切换 
+# 获得默认图的句柄
+g1 = tf.get_default_graph()
+# 要向一个图添加结点，得先把其设置为默认图
+g = tf.Graph()
+with g.as_default():
+    x = tf.add(3, 5)
+
+# sess = tf.Session(graph = g)
+with tf.Session(graph = g) as sess:
+    sess.run(x)
+
+
+# 建立多个图的好处是可以随意执行某个子图，而不是全图，而子图可以只执行某一op 
+# 同时也能将计算分解，方便求自动差分，和对分布式计算有帮助
+
+
